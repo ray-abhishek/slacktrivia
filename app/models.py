4473b0f203c3ade6cd3c955287f0fedc50c6a071
Charul090 <baricharul9@hotmail.com>
2020-06-24 21:05:21 +0530

    Added Intial DB

--- app/models.py
+++ app/models.py
@@ -14,3 +14,28 @@ class User(db.Model):
     orders = db.relationship('Order',backref='user',lazy=True)
     reviews = db.relationship('Review',backref='product',lazy=True)
 '''
+
+class Category(db.Model):
+    __tabelname__="category"
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(100))
+    setting=db.Column(db.String(100))
+    flag=db.Column(db.Boolean,nullable=False)
+
+
+class Question(db.Model):
+    __tablename__="question"
+    id=db.Column(db.Integer,primary_key=True)
+    question=db.Column(db.String(300))
+    option1=db.Column(db.String(100))
+    option2=db.Column(db.String(100))
+    option3=db.Column(db.String(100))
+    option4=db.Column(db.String(100))
+    answer=db.Column(db.String(100))
+
+
+class Question_Category(db.Model):
+    __tablename__="question_category"
+    id=db.Column(db.Integer,primary_key=True)
+    question_id=db.Column(db.Integer,db.ForeignKey("question.id"))
+    category_id=db.Column(db.Integer,db.ForeignKey("category.id"))
---
+++ queries.sql
@@ -0,0 +1,43 @@
+INSERT INTO category(name,flag) VALUES("Television",true);
+
+INSERT INTO category(name,flag) VALUES("Sports",true);
+
+INSERT INTO category(name,flag) VALUES("History",true);
+
+
+
+INSERT INTO question(question,option1,option2,option3,option4,answer) VALUES("Who played Chandler's father on Friends?","KATHLEEN TURNER","NATHAN LANE","MICHAEL DOUGLAS","ELLIOT GOULD","KATHLEEN TURNER");
+
+INSERT INTO question(question,option1,option2,option3,option4,answer) VALUES("Every episode of Seinfeld contains an image or reference to what superhero?","BATMAN","SUPERMAN","SPIDERMAN","PLASTIC MAN","SUPERMAN");
+
+INSERT INTO question(question,option1,option2,option3,option4,answer) VALUES("Who was the youngest person to host Saturday Night Live?","MACAULAY CULKIN","FRED SAVAGE","DREW BARRYMORE","MILEY CYRUS","DREW BARRYMORE");
+
+INSERT INTO question_category(question_id,category_id) VALUES(1,1);
+
+INSERT INTO question_category(question_id,category_id) VALUES(2,1);
+
+INSERT INTO question_category(question_id,category_id) VALUES(3,1);
+
+INSERT INTO question(question,option1,option2,option3,option4,answer) VALUES("What was the shortest war in human history?","ENGLAND AND ZANZIBAR","INDIA AND CHINA","USA AND CANADA","NORTH KOREA AND SOUTH KOREA","ENGLAND AND ZANZIBAR");
+
+INSERT INTO question(question,option1,option2,option3,option4,answer) VALUES("How many years did the 100 years war last?","100","200","50","116","116");
+
+INSERT INTO question(question,option1,option2,option3,option4,answer) VALUES("In which year was John F. Kennedy assassinated?","1978","1947","1963","1945","1963");
+
+INSERT INTO question_category(question_id,category_id) VALUES(4,3);
+
+INSERT INTO question_category(question_id,category_id) VALUES(5,3);
+
+INSERT INTO question_category(question_id,category_id) VALUES(6,3);
+
+INSERT INTO question(question,option1,option2,option3,option4,answer) VALUES("What club did retired footballer Zinedine Zidane play for during the 1997-98 season?","REAL MADRID","ARSENAL","JUVENTUS","MOHUN BAGAN","JUVENTUS");
+
+INSERT INTO question(question,option1,option2,option3,option4,answer) VALUES("How many NBA championships did Michael Jordan win with the Chicago Bulls?","15","24","9","6","6");
+
+INSERT INTO question(question,option1,option2,option3,option4,answer) VALUES("Which boxer inflicted Muhammad Ali’s first defeat in professional boxing?","George Foreman","Joe Frazier","Mike Tyson","Floyd Mayweather","Joe Frazier");
+
+INSERT INTO question_category(question_id,category_id) VALUES(7,2);
+
+INSERT INTO question_category(question_id,category_id) VALUES(8,2);
+
+INSERT INTO question_category(question_id,category_id) VALUES(9,2);
